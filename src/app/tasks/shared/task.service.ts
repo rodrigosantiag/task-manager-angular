96939f9f7a2add2634601074024d57ea1d914f15
Rodrigo Santiago <rodrigosantiag@gmail.com>
2019-04-08 05:07:22 -0300

    Getting task list

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -1,7 +1,11 @@
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
 import {Task} from './task.model';
+
 
 const TASKS: Array<Task> = [
   {id: 1, title: 'Fazer tarefa 1'},
@@ -16,18 +20,13 @@ const TASKS: Array<Task> = [
 @Injectable()
 
 export class TaskService {
+  public tasksUrl = 'api/tasks';
+
   public constructor(private http: Http) {}
 
-  public getTasks(): Promise<Task[]> {
-    const promise = new Promise((resolve, reject) => {
-      if (TASKS.length > 0) {
-        resolve(TASKS);
-      }else {
-        const error_msg = 'Não há tarefas';
-        reject(error_msg);
-      }
-    });
-    return promise;
+  public getTasks(): Observable<Task[]> {
+    return this.http.get(this.tasksUrl)
+      .map((response: Response) => response.json().data as Task[]);
   }
 
   public getImportantTasks(): Promise<Task[]> {
