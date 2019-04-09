02ebbc8a7ca7ccdf28664b1ce10525a874bdbf84
Rodrigo Santiago <rodrigosantiag@gmail.com>
2019-04-09 04:42:59 -0300

    Adding task search component base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,36 @@
 // Angular Imports
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {NgModule} from '@angular/core';
 
 // Components Imports
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {TaskDetailComponent} from './tasks/task-detail/task-detail.component';
+import {TaskSearchComponent} from './navbar/task-search/task-search.component';
+import {TasksComponent} from './tasks/tasks.component';
 
 // Services Imports
 import {TaskService} from './tasks/shared/task.service';
 
 // Modules Imports
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 
 // In memory web api
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {InMemoryTaskDataService} from './in-memory-task-data.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     NavbarComponent,
+    TaskDetailComponent,
+    TaskSearchComponent,
     TasksComponent,
-    TaskDetailComponent
   ],
   imports: [
     BrowserModule,
@@ -41,4 +44,5 @@ import {InMemoryTaskDataService} from './in-memory-task-data.service';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
