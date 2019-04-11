05734fd8b589f54e5216fe23ef15ccbb5511f899
Rodrigo Santiago <rodrigosantiag@gmail.com>
2019-04-10 21:26:06 -0300

    Improving task detail form

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -48,4 +48,8 @@ export class TaskDetailComponent implements OnInit {
       );
     }
   }
+
+  public showFieldError(field): boolean {
+    return field.invalid && ( field.touched || field.dirty );
+  }
 }
